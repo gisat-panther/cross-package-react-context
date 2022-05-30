b098813882b7150e644a5fa2f5708ba546f30cc9
Vojtěch Dubrovský <v.dubr@hotmail.com>
2022-05-30 14:55:05 +0200

    Add and apply prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from "react";
+import React from 'react';
 
 const Context = () => {
+	let contexts = {};
 
-    let contexts = {};
+	let createContext = (key, initialValue) => {
+		contexts[key] = React.createContext(initialValue);
+		return contexts[key];
+	};
 
-    let createContext = (key, initialValue) => {
-        contexts[key] = React.createContext(initialValue);
-        return contexts[key];
-    };
+	let getContext = (key, initialValue) =>
+		contexts[key] || createContext(key, initialValue);
 
-    let getContext = (key, initialValue) => contexts[key] || createContext(key, initialValue);
-
-    return {
-        getContext
-    }
+	return {
+		getContext,
+	};
 };
 
 export default Context();
-
